ac8ae4d70d5044d22092b6c3e3c4c907eae5e1bb
Matt Woodruff <mwoodruff@rfxsolutions.com>
2022-08-15 21:23:30 -0500

    initial commit

---
+++ pm2.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: 'Tailwind',
+      script: 'tailwindcss -w -i ./styles/app.css -o app/styles/app.css',
+      ignore_watch: ['.'],
+      env: {
+        NODE_ENV: 'development',
+      },
+    },
+    {
+      name: 'Lucid',
+      script: 'remix dev',
+      ignore_watch: ['.'],
+      env: {
+        NODE_ENV: 'development',
+      },
+    },
+  ],
+};
